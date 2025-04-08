2333279cccfdb05ab8df9d884f5c6a35f91a56b3
Linda Chen <lindachen@Lindas-MacBook-Pro.local>
2025-04-07 21:01:24 -0400

    Initial commit: Set up project with.gitignore

---
+++ functions.py
@@ -0,0 +1,48 @@
+import google.generativeai as genai
+import language_tool_python
+from deep_translator import GoogleTranslator
+import time
+import os
+import asyncio
+
+from dotenv import load_dotenv
+load_dotenv(dotenv_path='minimind.env')
+
+API_KEY = os.getenv('API_KEY')
+
+genai.configure(api_key=API_KEY)
+
+async def get_ai_response(task: str, text: str) -> str:
+    try:
+        model = genai.GenerativeModel("gemini-1.5-pro")
+        prompt = ""
+
+        if task == "grammar":
+            prompt = f"Check for grammar and spelling errors. Return a grammatically correct version of the text:\n\n{text}"
+        else:
+            prompt = text
+
+        #gen_start = time.time()
+        response = await asyncio.to_thread(model.generate_content, prompt)
+        #print(f"[{time.time() - gen_start:.2f}s] AI generation took")
+
+        return response.text if response.text else "Sorry, I couldn't generate a response."
+    except Exception as e:
+        print(f"❌ Error in AI request: {e}")
+        return f"Error: {e}"
+
+"""
+def translate_text(text: str, dest_language: str = "en") -> str:
+    try:
+        translator = GoogleTranslator(source="auto", target=dest_language)
+        return translator.translate(text)
+    except Exception as e:
+        return f"Error: {e}"
+""" 
+
+def translate_text(text: str, source_language: str, target_language: str) -> str:
+    try:
+        translator = GoogleTranslator(source=source_language, target=target_language)
+        return translator.translate(text)
+    except Exception as e:
+        return f"Error: {e}"
